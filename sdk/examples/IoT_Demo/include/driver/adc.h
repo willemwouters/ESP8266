bbaa4e31cd477711fa8de5cffd224c82d9989f71
willemwouters@gmail.com <willemwouters@gmail.com>
2014-11-30 18:01:04 +0100

    used one base for gcc/sdk via symlink in project bsk folder

--- sdk/examples/IoT_Demo/include/driver/adc.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ADC_H__
-#define __ADC_H__
-
-uint16 adc_read(void);
-
-#endif
--- sdk/examples/at/user/at_baseCmd.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AT_BASECMD_H
-#define __AT_BASECMD_H
-
-void at_exeCmdNull(uint8_t id);
-void at_setupCmdE(uint8_t id, char *pPara);
-void at_exeCmdRst(uint8_t id);
-void at_exeCmdGmr(uint8_t id);
-
-#endif
