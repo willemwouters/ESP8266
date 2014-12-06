ca7223ba05f7c8715b7580e79ac94eee98f0483f
willemwouters@gmail.com <willemwouters@gmail.com>
2014-12-06 15:47:18 +0100

    trying to remove some buildfiles

--- projects/sdk3_uart/modules/src/lwip/core/ipv4/.output/eagle/debug/obj/inet_chksum.d
+++ projects/sdk3_uart/modules/src/lwip/core/ipv4/.output/eagle/debug/obj/inet_chksum.d
@@ -1,9 +1,14 @@
-.output/eagle/debug/obj/inet_chksum.o .output/eagle/debug/obj/inet_chksum.d : inet_chksum.c ../../../include/lwip/opt.h \
- ../../../include/lwipopts.h ../../../include/lwip/debug.h \
- ../../../include/lwip/arch.h ../../../include/arch/cc.h \
- ../../../../include/c_types.h ../../../../include/ets_sys.h \
- ../../../../include/c_types.h ../../../../include/eagle_soc.h \
- ../../../../include/osapi.h \
+.output/eagle/debug/obj/inet_chksum.o .output/eagle/debug/obj/inet_chksum.d : inet_chksum.c \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/opt.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwipopts.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/debug.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/arch.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/arch/cc.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/include/c_types.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/include/ets_sys.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/include/c_types.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/include/eagle_soc.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/include/osapi.h \
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/include/string.h \
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/include/_ansi.h \
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/include/newlib.h \
@@ -14,6 +19,9 @@
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/include/sys/_types.h \
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/include/sys/lock.h \
  /home/wouters/GIT/willemwouters/ESP8266/gcc/xtensa-lx106-elf_02/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
- ../../../include/user_config.h ../../../include/lwip/inet_chksum.h \
- ../../../include/lwip/pbuf.h ../../../include/lwip/err.h \
- ../../../include/lwip/ip_addr.h ../../../include/lwip/def.h
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/user_config.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/inet_chksum.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/pbuf.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/err.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/ip_addr.h \
+ /home/wouters/GIT/willemwouters/ESP8266/projects/sdk3_uart/src/include/lwip/def.h
