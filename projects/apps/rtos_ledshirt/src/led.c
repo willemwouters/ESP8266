6e105522f05a1cacd55e33456df9eb0b19453059
wouters <wouters@wouters>
2015-10-04 00:17:58 +0200

    updated optimize flag

---
+++ projects/apps/rtos_ledshirt/src/led.c
@@ -0,0 +1,209 @@
+/*
+ * led.c
+ *
+ *  Created on: Oct 3, 2015
+ *      Author: wouters
+ */
+
+#include "espressif/esp_common.h"
+#include "espmissingincludes.h"
+#include "framedriver.h"
+#include "config.h"
+#include "ws2812.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lwipopts.h"
+#include "lwip/sockets.h"
+#include "lwip/udp.h"
+
+#define NORMAL 1
+#define FLICKER 2
+#define FLICKER_BUFFER 3
+
+static int brightness = 5;
+static int fontbackR = 0;
+static int fontbackG = 0;
+static int fontbackB = 0;
+static int fontR = 255;
+static int fontG = 255;
+static int fontB = 255;
+static int MODE = NORMAL;
+static int MODEFLASH = NORMAL;
+static int scroll = 1;
+
+static int activebuffer = 0;
+static int initCommand = 0;
+static int flashspeed = 2;
+static int textspeed = 1;
+static long textframeoffset = 0;
+
+
+int32 chip_id = 0;
+int i1= 0;
+int i2= 0;
+int i3 = 0;
+int i4 = 0;
+
+int FRAME_REFRESH_SPEED =  (1000 / FRAMESPEED); // lets assume 5us for framereset
+
+long framecount = 0;
+
+void init_led() {
+	chip_id = system_get_chip_id();
+	i1 = (chip_id & 0xff << 24);
+	i2= (chip_id & 0xff << 16);
+	i3= (chip_id & 0xff << 8);
+	i4= (chip_id & 0xff);
+}
+void refreshFrameCb() {
+
+	int flicker = 0;
+
+	if(activebuffer == 0) {
+		if(scroll != 1) {
+			textframeoffset = 0;
+		}
+		write_texttowall(0, 0, textframeoffset, fontR, fontG, fontB, fontbackR, fontbackG, fontbackB);
+	}
+
+	if(MODEFLASH == FLICKER && (framecount % (flashspeed + 1)) < ((flashspeed+2) / 2)) {
+		flicker = 1;
+	}
+
+	if(MODE == FLICKER_BUFFER && (framecount % (flashspeed * 2 )) == 0) {
+		if(activebuffer == 1) {
+			activebuffer = 2;
+		} else if(activebuffer == 2) {
+			activebuffer = 1;
+		}
+	}
+
+	if(framecount % flashspeed == 0) {
+		textframeoffset++;
+	}
+
+	framecount++;
+	WS2812CopyBuffer(get_startbuffer(activebuffer), (COLUMNS * ROWS * COLORS), flicker, brightness);
+	//ets_intr_lock();
+	vTaskDelay(20 / portTICK_RATE_MS);
+	taskENTER_CRITICAL();
+	setled(get_startbuffer(activebuffer), (COLUMNS * ROWS * COLORS), brightness);
+	taskEXIT_CRITICAL();
+	//ets_intr_unlock();
+}
+
+void sendid() {
+	char data[6] = {0xFE, i1 , i2, i3, i4, initCommand };
+	struct pbuf* b = pbuf_alloc(PBUF_TRANSPORT, 6, PBUF_RAM);
+	memcpy (b->payload, data, 6);
+	struct udp_pcb * pUdpConnection = udp_new();
+	udp_sendto(pUdpConnection, b, IP_ADDR_BROADCAST, 9090);
+	pbuf_free(b);
+	udp_disconnect(pUdpConnection);
+}
+
+
+
+void  handle_led_command(char * pusrdata, int length) {
+
+	int i = 0;
+
+	if(length > 2) {
+
+			switch (pusrdata[0]) {
+				case 0x00:
+					os_printf("Writing text data to buffer: 0 len%d\r\n", length-2);
+					write_textwall_buffer(0, &pusrdata[2], length-2);
+					textframeoffset = 0;
+					activebuffer = 0;
+					scroll = 1;
+					if(pusrdata[1] == 0) {
+						scroll = 0;
+					}
+					os_printf("Text:");
+					for(i = 2; i < length; i++) {
+						os_printf("%c", pusrdata[i]);
+					}
+					os_printf("\n");
+					break;
+				case 0x01:
+					writestream(pusrdata[1], &pusrdata[2], length-2);
+					activebuffer = pusrdata[1];
+					break;
+				case 0x02:
+					os_printf("Setting mode to flicker \r\n");
+					if(pusrdata[1] == 0x00) {
+						MODEFLASH = FLICKER;
+						os_printf("Setting mode to flicker buffer\r\n");
+					} else {
+						MODEFLASH = NORMAL;
+						os_printf("Setting mode to normal \r\n");
+					}
+					break;
+				case 0x03:
+					if(pusrdata[1] == 0x00) {
+						MODE = FLICKER_BUFFER;
+						os_printf("Setting mode to flicker buffer\r\n");
+					} else {
+						MODE = NORMAL;
+						os_printf("Setting mode to normal \r\n");
+					}
+					break;
+				case 0x04:
+					if(pusrdata[1] > 19) {
+						pusrdata[1] = 19;
+					}
+					if(pusrdata[1] < 0) {
+						pusrdata[1] = 0;
+					}
+					os_printf("Setting framespeed: %d \r\n", pusrdata[1]);
+					flashspeed = 20 - pusrdata[1];
+					break;
+				case 0x05:
+					os_printf("Setting dim: %d \r\n", pusrdata[1]);
+					brightness = pusrdata[1];
+					break;
+				case 0x06:
+					if(pusrdata[1] > 19) {
+						pusrdata[1] = 19;
+					}
+					if(pusrdata[1] < 0) {
+						pusrdata[1] = 0;
+					}
+					os_printf("Setting textspeed: %d \r\n", pusrdata[1]);
+					textspeed = 20 - pusrdata[1];
+					break;
+				case 0x07:
+					set_buffer(pusrdata[1], pusrdata[2], pusrdata[3], pusrdata[4]);
+					os_printf("Setting to val \r\n");
+					activebuffer = pusrdata[1];
+					break;
+				case 0x08:
+					fontR = pusrdata[1];
+					fontG = pusrdata[2];
+					fontB = pusrdata[3];
+					os_printf("Setting fontcolor \r\n");
+					break;
+				case 0x09:
+					fontbackR = pusrdata[1];
+					fontbackG = pusrdata[2];
+					fontbackB = pusrdata[3];
+					os_printf("Setting font back \r\n");
+					break;
+				case 0xFE:
+					initCommand++;
+					handle_led_command(&pusrdata[1], length-1);
+					break;
+				case 0xFF:
+					sendid();
+//					os_timer_disarm(&writeudpTimer);
+//					os_timer_setfn(&writeudpTimer,(os_timer_func_t *) sendchipid_package, pUdpConnection);
+//					os_timer_arm(&writeudpTimer, 5, 0);
+					break;
+				default:
+					break;
+			}
+
+		}
+	//pbuf_free(p);
+}
---
+++ projects/apps/rtos_ledshirt/src/user_main.c
@@ -0,0 +1,170 @@
+#include "espressif/esp_common.h"
+#include "espmissingincludes.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+#include "freertos/queue.h"
+#include "lwip/sockets.h"
+#include "lwip/init.h"
+#include "led.h"
+#include "gpio.h"
+
+#include "framedriver.h"
+#include "ws2812.h"
+
+#define EVT_TIMER  0x01
+#define EVT_DATA 0x02
+
+typedef struct {
+	uint8_t event_type;
+	char* data;
+	int len;
+} my_event_t;
+
+static xQueueHandle mainqueue;
+static xTimerHandle timerHandle;
+
+	char buffer[1024];
+
+	static void receive_udp(void *pvParameters) {
+		  int lSocket;
+		   struct sockaddr_in sLocalAddr, sDestAddr;
+
+		   int nbytes;
+		   int i;
+		   lSocket = lwip_socket(AF_INET, SOCK_DGRAM, 0);
+		   if(lSocket != 0) {
+			   printf("ERROR \r\n");
+		   }
+
+		   memset((char *)&sLocalAddr, 0, sizeof(sLocalAddr));
+		   memset((char *)&sDestAddr, 0, sizeof(sDestAddr));
+
+		   /*Destination*/
+		   sDestAddr.sin_family = AF_INET;
+		   sDestAddr.sin_len = sizeof(sDestAddr);
+		   sDestAddr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+		   sDestAddr.sin_port = htons(8080);
+
+		   /*Source*/
+		   sLocalAddr.sin_family = AF_INET;
+		   sLocalAddr.sin_len = sizeof(sLocalAddr);
+		   sLocalAddr.sin_addr.s_addr = htonl(INADDR_ANY); //inet_addr("192.168.4.1");
+		   sLocalAddr.sin_port = htons(8080);
+
+		   int err = lwip_bind(lSocket, (struct sockaddr *)&sLocalAddr, sizeof(sLocalAddr));
+		   if(err != 0) {
+			   printf("ERROR \r\n");
+		   }
+		   while (1) {
+				 nbytes=lwip_recv(lSocket, buffer, sizeof(buffer),8);
+				 if (nbytes>0){
+					//lwip_sendto(lSocket, buffer, nbytes, 0, (struct sockaddr *)&sDestAddr, sizeof(sDestAddr));
+					char * dat = malloc(nbytes);
+					memcpy(dat, buffer, nbytes);
+					dat[nbytes] = 0;
+					my_event_t ev;
+					ev.event_type = EVT_DATA;
+					ev.data = dat;
+					ev.len = nbytes;
+					xQueueSend(mainqueue, &ev, 0);
+				 }
+		   }
+		   lwip_close(lSocket);
+	}
+
+
+static void timer_cb(xTimerHandle xTimer) {
+	my_event_t ev;
+	ev.event_type = EVT_TIMER;
+	xQueueSend(mainqueue, &ev, 0);
+}
+
+void simple_task(void *pvParameters) {
+	// Received pvParameters is communication queue
+	xQueueHandle *com_queue = (xQueueHandle *) pvParameters;
+	printf("%s: Started user interface task\n", __FUNCTION__);
+	while (1) {
+		my_event_t ev;
+		xQueueReceive(*com_queue, &ev, portMAX_DELAY);
+
+		switch (ev.event_type) {
+		case EVT_TIMER:
+			//printf("%s: Received Timer Event\n", __FUNCTION__);
+			refreshFrameCb();
+			break;
+		case EVT_DATA:
+			handle_led_command(ev.data, ev.len);
+			free(ev.data);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+
+void setap(char * ssid, int len) {
+	static struct softap_config apconf;
+	wifi_set_opmode(STATIONAP_MODE);
+	wifi_softap_get_config(&apconf);
+	memset(apconf.ssid, 0, 32);
+	strncpy((char*) apconf.ssid, ssid, 32);
+	apconf.authmode = AUTH_OPEN;
+	apconf.max_connection = 20;
+	apconf.ssid_hidden = 0;
+	apconf.ssid_len = strlen(ssid);
+	apconf.channel = 1;
+
+	wifi_softap_set_config(&apconf);
+}
+
+void ICACHE_FLASH_ATTR connectToAp() {
+	char * ap = "LedAccess";
+	char * pass = "test12345";
+	wifi_set_phy_mode( PHY_MODE_11N );
+
+	struct station_config apconf;
+	wifi_station_set_auto_connect(true);
+	wifi_set_opmode(STATION_MODE);
+	wifi_station_get_config(&apconf);
+	strncpy((char*)apconf.ssid, ap, 32);
+	printf("connecting to: %s", apconf.ssid);
+	strncpy((char*)apconf.password, pass, 64);
+	wifi_station_set_config(&apconf);
+
+	//wifi_set_event_handler_cb(wifi_event_cb);
+}
+void user_init(void) {
+	uart_div_modify(0, UART_CLK_FREQ / 115200);
+	os_delay_us(500);
+    printf("SDK version : %s\n", system_get_sdk_version());
+	mainqueue = xQueueCreate(10, sizeof(my_event_t));
+
+	connectToAp();
+	//setap("test", 4);
+
+	init_led();
+
+	PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
+	PIN_PULLUP_DIS(PERIPHS_IO_MUX_GPIO5_U); // disable pullodwn
+	GPIO_REG_WRITE(GPIO_ENABLE_W1TS_ADDRESS,BIT5);
+	GPIO_OUTPUT_SET(GPIO_ID_PIN(5), 1);
+
+	char outbuffer[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	WS2812OutBuffer( outbuffer, 6 , 0); //Initialize the output.
+	write_textwall_buffer(0, "BIER ", 5);
+
+
+	xTaskCreate(simple_task, (signed char * )"simple_task", 256, &mainqueue, tskIDLE_PRIORITY+1, NULL);
+	xTaskCreate(receive_udp, (signed char * )"test", 256, &mainqueue, tskIDLE_PRIORITY, NULL);
+
+	timerHandle = xTimerCreate((signed char *) "Trigger", 150 / portTICK_RATE_MS, pdTRUE, NULL, timer_cb);
+	if (timerHandle != NULL) {
+		if (xTimerStart(timerHandle, 0) != pdPASS) {
+			printf("%s: Unable to start Timer ...\n", __FUNCTION__);
+		}
+	} else {
+		printf("%s: Unable to create Timer ...\n", __FUNCTION__);
+	}
+}
