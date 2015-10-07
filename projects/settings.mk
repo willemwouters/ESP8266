7f0c0eab9f289a8c8205dc348ac36fc69256ef96
wouters <wouters@wouters>
2015-10-07 22:43:01 +0200

    finally got rid of seperate modules make

--- projects/settings.mk
+++ projects/settings.mk
@@ -3,7 +3,7 @@ BUILD_BASE	= build
 FW_BASE		= firmware
 
 ESPTOOL		?= esptool.py
-ESPPORT		?= /dev/ttyUSB0
+ESPPORT		?= /dev/ttyUSB1
 
 FW_TOOL		?= /usr/bin/esptool
 
@@ -12,3 +12,13 @@ CC		:= $(TOOLCHAIN_BASE)/bin/xtensa-lx106-elf-gcc
 CPP		:= $(TOOLCHAIN_BASE)/bin/xtensa-lx106-elf-g++
 AR		:= $(TOOLCHAIN_BASE)/bin/xtensa-lx106-elf-ar
 LD		:= $(TOOLCHAIN_BASE)/bin/xtensa-lx106-elf-gcc
+
+
+
+ifndef OPTIMIZE_FLAG
+    OPTIMIZE_FLAG :=-O2
+endif
+# compiler flags using during compilation of source files
+CFLAGS		:= $(CFLAGS) -c $(OPTIMIZE_FLAG) -ggdb -std=gnu99 -Wpointer-arith -Wundef  -Wl,-EL -fno-inline-functions -nostdlib  -fno-exceptions -mlongcalls -mtext-section-literals  -D__ets__ -DICACHE_FLASH
+
+CPPFLAGS := -g $(OPTIMIZE_FLAG) -Wpointer-arith -Wundef -Wl,-EL -fno-inline-functions -nostdlib -mlongcalls -mtext-section-literals -fno-exceptions -fno-rtti  -D__ets__ -DICACHE_FLASH
